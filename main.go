bc78f882402daa7593351b26b40ee3f5d727567a
Alan2229 <rapilbek.alan@bk.ru>
2024-10-01 21:35:05 +0300

    New generation

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strconv"
+
+	"github.com/google/uuid"
 )
 
 type Book struct {
@@ -12,10 +14,10 @@ type Book struct {
 }
 
 type TakeBook interface {
-	TakeBookById(id int) Book
+	TakeBookById(id string) Book
 	TakeBookByName(name string) Book
 	TakeBooksByName(name ...string) []Book
-	TakeBooksById(id ...int) []Book
+	TakeBooksById(id ...string) []Book
 }
 
 type Searcher interface {
@@ -37,7 +39,7 @@ type Library interface {
 type MyLibrary struct {
 	books     []Book
 	booksName map[string]Book
-	booksId   map[int]Book
+	booksId   map[string]Book
 	lastId    string
 	address   string
 }
@@ -46,15 +48,17 @@ func NewLibrary(Address string) *MyLibrary {
 	return &MyLibrary{
 		books:     make([]Book, 0),
 		booksName: make(map[string]Book),
-		booksId:   make(map[int]Book),
-		lastId:    "0",
-		address:   Address,
+		booksId:   make(map[string]Book),
+		//обычный способ
+		//lastId:    "0",
+		//через UUID
+		lastId:  uuid.NewString(),
+		address: Address,
 	}
 }
 
-func (b *MyLibrary) TakeBookById(Id int) Book {
-	strId := strconv.Itoa(Id)
-	_, ok := b.Search(strId)
+func (b *MyLibrary) TakeBookById(Id string) Book {
+	_, ok := b.Search(Id)
 	if !ok {
 		fmt.Println("Book with this Id does not exist")
 		fmt.Println("Do you want to try again with the correct Id?")
@@ -62,7 +66,7 @@ func (b *MyLibrary) TakeBookById(Id int) Book {
 		k := ""
 		fmt.Scan(&k)
 		if k == "Y" {
-			q := 0
+			q := ""
 			fmt.Scan(&q)
 			return b.TakeBookById(q)
 		} else {
@@ -70,7 +74,7 @@ func (b *MyLibrary) TakeBookById(Id int) Book {
 		}
 	} else {
 		for i := 0; i < len(b.books); i++ {
-			if b.books[i].Id == strId {
+			if b.books[i].Id == Id {
 				w := b.books[i]
 				b.books[i] = b.books[len(b.books)-1]
 				b.books[len(b.books)-1] = Book{}
@@ -90,18 +94,22 @@ func (b *MyLibrary) AddBook(books ...SimBook) {
 		var t Book
 		t.Author = val.Author
 		t.BookName = val.BookName
+
 		//Обычный способ генерирование уникального символа
+		/*
+			t.Id = b.lastId
+			intId, _ := strconv.Atoi(b.lastId)
+			b.booksId[intId] = t
+			intId++
+			lId := strconv.Itoa(intId)
+			b.lastId = lId
+		*/
+
+		// Способ через UUID
 		///*
 		t.Id = b.lastId
-		intId, _ := strconv.Atoi(b.lastId)
-		b.booksId[intId] = t
-		intId++
-		lId := strconv.Itoa(intId)
-		b.lastId = lId
-		//*/
-		// Способ
-		///*
-
+		var x = uuid.New().String()
+		b.lastId = x
 		//*/
 		b.booksName[val.BookName] = t
 		b.books = append(b.books, t)
@@ -121,7 +129,7 @@ func (b *MyLibrary) Search(id string) (*Book, bool) {
 	if err != nil {
 		return nil, false
 	}
-	val, ok := b.booksId[intId]
+	val, ok := b.booksId[strconv.Itoa(intId)]
 	if !ok {
 		return nil, false
 	}
@@ -133,12 +141,12 @@ func (b *MyLibrary) TakeBookByName(name string) Book {
 	if ok {
 		for i := 0; i < len(b.books); i++ {
 			if b.books[i].BookName == name {
-				w, _ := strconv.Atoi(b.books[i].Id)
-				b.books[i] = b.books[len(b.books)-1]
+				w := t.Id
+				b.books[i] = b.books[int(len(b.books))-1]
 				b.books[len(b.books)-1] = Book{}
-				b.books = b.books[:len(b.books)-1]
+				b.books = b.books[:int(len(b.books))-1]
 				delete(b.booksId, w)
-				delete(b.booksName, b.books[i].BookName)
+				delete(b.booksName, t.BookName)
 				break
 			}
 		}
@@ -155,7 +163,7 @@ func (b *MyLibrary) TakeBooksByName(names ...string) []Book {
 	return ans
 }
 
-func (b *MyLibrary) TakeBooksById(ids ...int) []Book {
+func (b *MyLibrary) TakeBooksById(ids ...string) []Book {
 	var ans []Book
 	for i := 0; i < len(ids); i++ {
 		ans = append(ans, b.TakeBookById(ids[i]))
@@ -167,6 +175,6 @@ func main() {
 	var t Library
 	t = NewLibrary("some address")
 	t.AddBook(SimBook{"1984", "George Orwell"})
-	fmt.Println(t.TakeBookById(0))
+	fmt.Println(t.TakeBookByName("1984"))
 	fmt.Println(1000000007)
 }
